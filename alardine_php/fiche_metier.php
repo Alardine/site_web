c544eb17216dce0cd4a972ed9f748097830c4a70
tykrasta <tykrastagames@gmail.com>
2017-11-28 12:09:03 +0100

    fiche metier

---
+++ alardine_php/fiche_metier.php
@@ -0,0 +1,7 @@
+<?php require 'inc/header.php' ?>
+<h2>Fiche métier</h2>
+<a href="OFFRE_ALARDINE_Analyste_risque_AMF.pdf" class="btn btn-primary">Analyste risque AMF</a>
+
+
+
+<?php require 'inc/footer.php' ?>
