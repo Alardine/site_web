0a3d3b8e4f18c493df55430fdf2ada2f884cf0f6
Votre nom ou pseudo <Votre@email.com>
2017-11-28 10:53:23 +0100

    vrai 1er version

---
+++ alardine_php/nous.php
@@ -0,0 +1,104 @@
+<?php require 'inc/header.php' ?>
+
+<section id="nous">
+    <section id="vision">
+        <h2 id="v">Notre vision</h2>
+
+        <blockquote>
+            <p>Après avoir passé 10 années dans la Finance, je décide en 2009 de me tourner vers le métier de l’enseignement.
+                Professeur d’Economie/Gestion dans les lycées en Seine Saint Denis, je me suis souvent retrouvé face à des
+                élèves plein d’énergies, créatifs mais désabusé(e)s quant à leur manque de perspectives professionnelles.</p>
+            <p>Témoin de ce constat, je décide de créer en 2011,
+                <strong>Alardine</strong> (A la Recherche de Nouvelles Energies) afin de donner les moyens, la passerelle aux candidats
+                motivés d’intégrer le secteur de la Banque et aussi permettre aux Banques d’accéder à de nouvelles énergies.</p>
+            <cite>Rachid Belabbas, Président</cite>
+        </blockquote>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/rfZCfUHiM84?rel=0&amp;showinfo=0;autoplay=1" frameborder="0" allowfullscreen></iframe>
+        <blockquote>
+            «La diversité est au cœur même de notre capacité à servir nos clients et à maximiser le rendement pour nos actionnaires.
+            Diversité soutient et renforce la culture de l’entreprise, et cela renforce notre réputation en tant qu’employeur
+            de choix dans notre secteur et au-delà ».
+            <cite>Lloyd C. Blankfein, Président du conseil et chef de la direction The Goldman Sachs Group, Inc</cite>
+        </blockquote>
+    </section>
+    <h2 id="e">Notre équipe</h2>
+    <section id="equipe">
+
+        <article>
+            <img src="img/rachid_belabbas.jpg" alt="rachid_belabbas">
+            <h3>Rachid Belabbas</h3>
+            <h4>Président, formateur certifié Banque Finance</h4>
+            <p>Master spécialisé en Ingénierie Financière/DEA Analyse économique. Après avoir passé plus de 10 ans dans la Finance
+                de marché (départements Ingénierie Financière (IPO), Securities Lending, gestion d’actifs), puis exercé en
+                tant que Professeur dans l’Education Nationale. J’ai fondé Alardine afin de répondre aux nouveaux défis du
+                monde numérique source de nouvelles opportunités économiques. Aujourd’hui, j’interviens dans les formations
+                Banque/Finance.
+                <br>« Allions productivité et excellence pour pérenniser nos résultats »</p>
+          <!--  <a href="#">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+            </a>    -->
+            <a href="https://fr.linkedin.com/in/rachidbelabbas">    
+                <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+            </a>
+        </article>
+        <article>
+            <img src="img/alyssa_cutman.jpg" alt="alyssa_cutman">
+            <h3>Alyssa Cutman</h3>
+            <h4>Directrice d'innovation & partenariats</h4>
+            <p>Titulaire d’un Diplôme d’Université de formateur d’adultes et d’un master chef de projet digital learning, j’ai
+                acquis une expérience professionnelle de plus de 6 ans en relation client omni-canal. Ma connaissance du
+                terrain à travers mon parcours ascendant et mes appétences relationnelles sont de véritables facteurs de
+                réussite et de performance. Aujourd’hui, chargée de développer les dispositifs de la formation sur le plan
+                digital, je gère l'ensemble des projets en garantissant les délais et la qualité des réalisations.
+                <br>« Soyons créatifs, nous accroîtrons votre potentiel ! »</p>
+           <!-- <a href="#">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+            </a> -->
+            <a href="https://fr.linkedin.com/in/alyssacutman">
+                <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+            </a>
+        </article>
+        <article>
+            <img src="img/riad_khennaoui.png" alt="riad_khennaoui">
+            <h3>Riad Khennaoui</h3>
+            <h4>Responsable recrutement</h4>
+            <p>​Issu d’une école de commerce en Management-Banque-Assurance, j’ai passé plus de 7 ans dans le domaine de la
+                relation clients (banque, télémarketing, recouvrement). J’ai rapidement intégré le milieu professionnel,
+                et successivement occupé différents postes qui me permettent aujourd’hui de bien connaitre les attentes des
+                recruteurs, de repérer les talents et ainsi répondre efficacement à leurs exigences. Aimer être en contact
+                avec de nouvelles personnes et prendre le temps d’échanger représentent à mes yeux les qualités premières
+                pour recruter.
+                <br> </p>
+
+            <!--<a href="#">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+            </a> -->
+            <a href="https://www.linkedin.com/in/riad-khennaoui-b3619772/">
+                <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+            </a>
+        </article>
+    </section>
+    <section id="temoignage">
+    <h2 id="t">Temoignages</h2>
+    <div class="slider">
+			<div id="slider">
+				<div>
+					<article>
+                    <iframe width="800" height="500" src="https://www.youtube.com/embed/_wtWou84vNo?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+					</article>
+				</div> 
+				<div>
+					<article>
+                    <iframe width="800" height="500" src="https://www.youtube.com/embed/T04ZZIWpEVg?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+					</article>
+				</div>
+				<div>
+					<article>
+					<iframe width="800" height="500" src="https://www.youtube.com/embed/1Jayv9uVWFI?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>	
+					</article>
+				</div>
+			</div>
+		</div>
+    </section>
+</section>
+<?php require 'inc/footer.php' ?>
